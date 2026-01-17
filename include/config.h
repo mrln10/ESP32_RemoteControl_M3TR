6d188e4e38f0e1a56c09de8cb773db1ec9eee511
mrln10 <berbuirmarlon@web.de>
2026-01-17 23:27:42 +0100

    changed config.h to work with esp32-eth01 aswell as platformio.ini to correctly compile the firmware. deleted bl activation in TFTDisplay.cpp since it is permanently soldered to 3,3V. With the ETH01 it is importand to solder 10k pullups to the data pins of the encoder since the pins on the eth01 are input only and dont have the option to use an internal pullup

--- include/config.h
+++ include/config.h
@@ -1,21 +1,40 @@
 #pragma once
 
-// SPI Pins (ESP32 Standard)
-#define TFT_SCK   18  //SCK
-#define TFT_MOSI  23  //SDA
+//FÜR ESP 32
 
-// Display Steuerpins
-#define TFT_CS    5
-#define TFT_DC    16
-#define TFT_RST   17 //RES
-#define TFT_BL    4
+//// SPI Pins (ESP32 Standard)
+//#define TFT_SCK   18  //SCK
+//#define TFT_MOSI  23  //SDA
+//
+//// Display Steuerpins
+//#define TFT_CS    5
+//#define TFT_DC    16
+//#define TFT_RST   17 //RES
+//#define TFT_BL    4
+//
+//// Rotary Encoder
+//#define ENC_CLK  32
+//#define ENC_DT   33
+//#define ENC_SW   25
+//
+//// Navigation Buttons ,gegen GND -> INPUT_PULLUP
+//#define BTN_LEFT    26
+//#define BTN_RIGHT  27
 
-// Rotary Encoder
-#define ENC_CLK  32
-#define ENC_DT   33
-#define ENC_SW   25
+//Für ETH01
 
-// Navigation Buttons ,gegen GND -> INPUT_PULLUP
-#define BTN_LEFT    26
-#define BTN_RIGHT  27
+// ST7735 (SPI)
+#define TFT_SCK   14
+#define TFT_MOSI  15
+#define TFT_CS     4
+#define TFT_DC    12
+#define TFT_RST   2
+// TFT_BL -> direkt an 3V3 (nicht an GPIO)
+
+#define ENC_CLK  36
+#define ENC_DT   39
+#define ENC_SW   35   // gegen GND, INPUT_PULLUP
+
+#define BTN_LEFT   5
+#define BTN_RIGHT 17
 
